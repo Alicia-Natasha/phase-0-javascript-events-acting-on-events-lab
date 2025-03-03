84008010180f40d1ae3ec3193171037330a70a1d
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-03 17:04:01 +0300

    phase-0-javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerLeft() {
+    let dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let dodger = document.getElementById('dodger');
+    let right = parseInt(dodger.style.left);
+
+    if (right < 790) {
+        dodger.style.left = `${right + 10}px`;
+    }
+}
